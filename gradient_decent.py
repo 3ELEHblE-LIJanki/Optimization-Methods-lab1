b218eeb4889b98ce7287c9278617a89ada02f63c
l3xu5 <l3xu5.p@gmail.com>
2025-03-22 19:18:53 +0300

    fix docs

--- gradient_decent.py
+++ gradient_decent.py
@@ -1,32 +1,30 @@
 from typing import Callable
 from lrs import LRS
 
-'''
-    Класс реализующий поиск максимума и минимума на основе градиентного спуска и переданных параметров
-'''
-class GradientDecent:
 
-    '''
-        learning_rate_scheduling - выбранная модель поиска шага
-        max_iterations - максимальное число итераций (чтобы не зациклиться)
-    '''
+class GradientDecent:
+    """
+        Класс реализующий поиск максимума и минимума на основе градиентного спуска и переданных параметров
+    """
 
     def __init__(self, learning_rate_scheduling: LRS, max_iterations: int):
+        """
+            learning_rate_scheduling - выбранная модель поиска шага
+            max_iterations - максимальное число итераций (чтобы не зациклиться)
+        """
         self.learning_rate_scheduling = learning_rate_scheduling
         self.max_iterations = max_iterations
 
-    '''
-        f - исследуемая функция
-
-        return - всё что необходимо, допишем когда графики будем рисовать
-    '''
     def find_min(self, f: Callable[[tuple], float]):
+        """
+            f - исследуемая функция
+            return - всё что необходимо, допишем когда графики будем рисовать
+        """
         ...
 
-    '''
-        f - исследуемая функция
-
-        return - всё что необходимо, допишем когда графики будем рисовать
-    '''
     def find_max(self, f: Callable[[tuple], float]):
+        """
+            f - исследуемая функция
+            return - всё что необходимо, допишем когда графики будем рисовать
+        """
         ...
