593a448c2fbefbda7e2b679ac95f4e075e20e711
Alina12345 <Alina.Khalili@yandex.ru>
2025-03-25 03:31:12 +0300

    add 2 lrs-f + start test

--- lrs.py
+++ lrs.py
@@ -1,10 +1,14 @@
 from typing import Callable
 import math
 
+import numpy as np
+
+EPS = 1e-5
+
 def diff(f, x, eps, index: int):
-        x_i = x.copy()
-        x_i[index] += eps
-        return (f(x_i) - f(x)) / eps
+    x_i = x.copy()
+    x_i[index] += eps
+    return (f(x_i) - f(x)) / eps
 
 def gradient(f, x, eps):
     grad = []
@@ -43,7 +47,7 @@ def armiho(c1: float, q: float) -> LRS:
     return lambda x, k, f: _arm(c1, q, x, k, f)
 
 def _arm(c1, q, x, k, f):
-    ff = gradient(f, x, 0.00001)
+    ff = gradient(f, x, EPS)
     fff = [-ff_i for ff_i in ff]
     l = lambda a: f(x) + c1 * a * mult(fff, ff)
     a = 500000
@@ -59,8 +63,41 @@ def _arm(c1, q, x, k, f):
     
     return - LRS (learning rate scheduling) по правилу Вольфе с заданными гипер-параметрами 
 '''
-def wolfe(c1: float, c2: float) -> LRS:
-    ...
+def wolfe(c1: float = 1e-4, c2: float = 0.9) -> LRS:
+    return lambda x, k, f: _wolfe(c1, c2, x, k, f)
+
+
+def _wolfe(c1, c2, x, k, f):
+    max_iterations = 100
+    a = 1
+
+    fx = f(x)
+    grad = lambda x: np.array(gradient(f, x, EPS))
+    grad_x_np = grad(x)
+
+    # (1-Armijo) f(x + a * p) <= f(x) + c1 * a * graf(f)^T * p
+    # (2-Curvature) grad(f(x + ap))^T * p >= c2 * grad(f(x))^T * p
+
+    p = -grad_x_np
+    right = grad_x_np.dot(p)  # grad(f(x))^T p - где p - прошлое направление - в моём случае -grad (??)
+
+    for _ in range(max_iterations):
+        x_new = np.array(x) + a * p
+        fx_new = f(x_new.tolist())
+        grad_new = grad(x_new)
+
+        # Проверка 1
+        if fx_new > fx + c1 * a * right:
+            a *= 0.5
+            continue
+
+        # Проверка 2
+        if grad_new.dot(p) < c2 * right:
+            a *= 1.5
+            continue
+        return a
+    return a
+
 
 '''
     Правило Голдстейна
@@ -70,8 +107,41 @@ def wolfe(c1: float, c2: float) -> LRS:
 
     return - LRS (learning rate scheduling) по правилу Голдстейна с заданными гипер-параметрами 
 '''
-def goldstein(c1: float, c2: float) -> LRS:
-    ...
+def goldstein(c1: float = 0.1, c2: float = 0.9) -> LRS:
+    return lambda x, k, f: _goldstein(c1, x, k, f)
+
+
+def _goldstein(c1, x, k, f):
+    max_iterations = 100
+    a = 1
+
+    fx = f(x)
+    grad = lambda x: np.array(gradient(f, x, EPS))
+    grad_x_np = grad(x)
+
+    # (1) f(x + a * p) <= f(x) + c1 * a * graf(f)^T * p
+    # (2) f(x + a * p) >= f(x) + (1 - c1) * a * graf(f)^T * p
+
+    p = -grad_x_np
+    right = grad_x_np.dot(p)  # grad(f(x))^T p - где p grad (??)
+
+    for _ in range(max_iterations):
+        x_new = np.array(x) + a * p
+        fx_new = f(x_new.tolist())
+
+        # Проверка 1
+        if fx_new > fx + c1 * a * right:
+            a *= 0.5
+            continue
+
+        grad_new = grad(x_new)
+        # Проверка 2
+        if fx_new < (1 - c1) * right:
+            a *= 1.5
+            continue
+        return a
+    return a
+
 
 '''
     Постоянный метод планирования шага
@@ -92,7 +162,7 @@ def constant(h0: float) -> LRS:
     return - Функциональный LRS (learning rate scheduling) с заданными гипер-параметрами 
 '''
 def exponential_decay(h0: float, l: float) -> LRS:
-    return lambda x, k, f: h0 * math.e**(-l*k) + impulse(k)
+    return lambda x, k, f: h0 * math.e ** (-l * k) + impulse(k)
 
 '''
     Функциональный метод планирования шага (Полиномиальное затухание)
@@ -103,4 +173,4 @@ def exponential_decay(h0: float, l: float) -> LRS:
     return - Функциональный LRS (learning rate scheduling) с заданными гипер-параметрами 
 '''
 def polynomial_decay(a: float, b: float) -> LRS:
-    return lambda x, k, f: (1.0 / math.sqrt(k + 1)) * (b * k + 1)**(-a) + impulse(k)
+    return lambda x, k, f: (1.0 / math.sqrt(k + 1)) * (b * k + 1) ** (-a) + impulse(k)
